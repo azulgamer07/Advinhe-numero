f82b52e24f828c58498d2fa9d6d8b7eac5aee96b
Fernando Carneiro <fernandocarneirovasconcelos@gmail.com>
2026-05-01 11:17:52 -0300

    desafio 1:  Contador de tentativas

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-let numeroSecreto = Math.floor(Math.random() * 100) + 1;
+let numeroSecreto = 10;
+// Math.floor(Math.random() * 100) + 1;
+let tentativas = 0;
 
 function verificarPalpite() {
     let palpite = document.getElementById("palpite").value;
     let mensagem = document.getElementById("mensagem");
+    tentativas++;
 
 
     if (palpite == numeroSecreto) {
-        mensagem.innerText = "Parabéns! Você acertou!";
-    }
-    else if (palpite < numeroSecreto) {
-        mensagem.innerText = "Tente um número maior!";
-    }
-    else {
-        mensagem.innerText = "Tente um número menor!";
-    }
+            mensagem.innerText = `Parabéns! Você acertou o número secreto em ${tentativas} tentativas!`;
+        }
+        else if (palpite < numeroSecreto) {
+            mensagem.innerText = "Tente um número maior!";
+        }
+        else if (palpite > numeroSecreto) {
+            mensagem.innerText = "Tente um número menor!";
+        } 
 }
+
